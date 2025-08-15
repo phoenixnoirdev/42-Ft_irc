ab6143f5336be3f86c0e01ecbea2d961faadab75
Lolipopcrash <kevin.leveque01@gmail.com>
2025-08-15 12:45:02 +0200

    Modifier class user pour avoir une liste chainee, doit encore coder le constructeur qui va se construire sur base d'un JSON. Pas encore tester les fonctionalites du  code.

--- irc/inc/user.hpp
+++ irc/inc/user.hpp
@@ -15,38 +15,94 @@
 
 #include <string>
 
+typedef struct	s_user
+{
+	std::string		uuid;
+	std::string		name;
+	std::string		nick;
+	std::string		pass;
+	int				grade;
+	struct s_user	*next;
+
+	t_user(	const std::string& uuid_,
+			const std::string& name_,
+			const std::string& nick_,
+			const std::string& pass_,
+			int grade_):
+		uuid(uuid_),
+		name(name_),
+		nick(nick_),
+		pass(pass_),
+		grade(grade_),
+		next(NULL)
+	{}
+	t_user(const t_user& other):
+		uuid(other.uuid),
+		name(other.name),
+		nick(other.nick),
+		pass(other.pass),
+		grade(other.grade),
+		next(NULL)
+	{}
+}					t_user;
+
 class User
 {
-    private:
-        std::string _Name;
-        std::string _Nick;
-        int _Grade;
+	private:
+		t_user	*_top;
+		t_user	*_bot;
+		size_t	_size;
 
-    public:
-        //=================
-        //Constructeur
-        //=================
-        User(std::string name, std::string nick, int grade);
-        ~User();
+		//===============
+		//Fonctions private
+		//===============
 
+	public:
+		//=================
+		//Canonical Form
+		//=================
+		User();
+		User(const User& other);
+		User&	operator=(const User& other);
+		~User();
 
-        //===============
-        //Fonctions public
-        //===============
-        std::string GetName();
-        void SetName(std::string name);
+		//=================
+		//Constructeur
+		//=================
+		User(t_user *list);
+		User(const std::string& json);
 
-        std::string GetNick();
-        void SetNick(std::string nick);
+		//===============
+		//Fonctions public
+		//===============
+		void	addUser(const std::string& uuid_,
+						const std::string& name_,
+						const std::string& nick_,
+						const std::string& pass_,
+						int grade_);
+		void	addUser(const t_user& other);
 
-        int GetGrad();
-        void SetGrade(int grade);
+		//Deletion
+		void	clear();
+		bool	removeUserByUUID(const std::string& uuid);
 
+		//Information Retrieval
+		size_t	getSize() const;
+		t_user	*getTop() const;
+		t_user	*getBot() const;
+		t_user	*getUserByIndex(int index) const;
+		t_user	*getUserByUUID(const std::string& uuid) const;
+		t_user	*getUserByName(const std::string& name) const;
+		t_user	*getUserByNick(const std::string& nick) const;
+		t_user	*getUserByPass(const std::string& pass) const;
+		t_user	*getUserByUUID(const std::string& uuid, int *id) const;
 
-        //===============
-        //Fonctions private
-        //===============
-        
+		//Information Verification
+		bool	empty() const;
+		bool	hasUUID(const std::string& uuid) const;
+		bool	hasName(const std::string& name) const;
+		bool	hasNick(const std::string& nick) const;
+		bool	hasPass(const std::string& pass) const;
 };
 
 #endif //USER.HPP
--- irc/src/user.cpp
+++ irc/src/user.cpp
@@ -13,67 +13,318 @@
 #include "../inc/inc.hpp"
 
 //=================
+//Canonical Form
+//=================
+
+User::User(): _top(NULL), _bot(NULL), _size(0) {}
+
+User::User(const User& other): _top(NULL), _bot(NULL), _size(0)
+{
+	t_user	*tmp;
+	size_t	len;
+
+	len = other.getSize();
+	tmp = other.getTop();
+	while (_size < len && tmp != NULL)
+	{
+		addUser(t_user(tmp));
+		tmp = tmp->next;
+		_size++;
+	}
+}
+
+User&	User::operator=(const User& other)
+{
+	if (this != other)
+	{
+		t_user	*tmp;
+		size_t	len;
+
+		len = other.getSize();
+		tmp = other.getTop();
+		_size = 0;
+		while (_size < len && tmp != NULL)
+		{
+			addUser(t_user(tmp));
+			tmp = tmp->next;
+			_size++;
+		}
+	}
+	return (*this);
+}
+
+~User::User() {clear();}
+
+//=================
 //Constructeur
 //=================
 
-User::User(std::string name, std::string nick, int grade)
+User::User(t_user *list): _top(NULL), _bot(NULL), _size(0)
 {
-    this->_Name = name;
-    this->_Nick = nick;
-    this->_Grade = grade;
+	t_user	*tmp;
 
-
-    std::cout << YELLOW << "------------------------------------" << RESET << std::endl;
-    std::cout << GREEN << "New user" << RESET << std::endl;
-    std::cout << YELLOW << "-- Name:" << CYAN  << this->_Name << RESET << std::endl;
-    std::cout << YELLOW << "-- Nick: " << CYAN << this->_Nick <<  RESET << std::endl;
-    std::cout << YELLOW << "-- Grade: " << CYAN << this->_Grade <<  RESET << std::endl;
-    std::cout << YELLOW << "------------------------------------" << RESET << std::endl;   
-
-    return ;
+	tmp = other.getTop();
+	while (tmp != NULL)
+	{
+		addUser(t_user(tmp));
+		tmp = tmp->next;
+		_size++;
+	}
 }
 
-
-User::~User()
+User::User(const std::string& json): _top(NULL), _bot(NULL), _size(0)
 {
-    
-    return ;
+
 }
 
 //===============
 //Fonctions public
 //===============
 
-std::string User::GetName()
+void	User::addUser(
+	const std::string& uuid_,
+	const std::string& name_,
+	const std::string& nick_,
+	const std::string& pass_,
+	int grade_)
 {
-    return this->_Name;
+	t_user	*tmp = new t_user(uuid_, name_, nick_, pass_, grade_);
+
+	if (empty())
+		_top = tmp;
+	else
+		_bot->next = tmp;
+	_bot = tmp;
+	_size++;
 }
 
-void User::SetName(std::string name)
+void	User::addUser(const t_user& other)
 {
-    this->_Name = name;;
+	t_user	*tmp = new t_user(other);
+
+	if (empty())
+		_top = tmp;
+	else
+		_bot->next = tmp;
+	_bot = tmp;
+	_size++;
 }
 
-
-std::string User::GetNick()
+void	User::clear()
 {
-    return this->_Nick;
+	t_user	*tmp;
+
+	while (top)
+	{
+		tmp = _top;
+		_top = _top->next;
+		delete tmp;
+		_size--;
+	}
+	_bot = NULL;
 }
 
-void User::SetNick(std::string nick)
+bool	User::removeUserByUUID(const std::string& uuid)
 {
-    this->_Nick = nick;
+	t_user	*tmp;
+	t_user	*prev;
+	int		i = 0;
+
+	tmp = getUserByUUID(uuid, &i);
+	prev = getUserByIndex(i - 1);
+	prev->next = tmp->next;
+	delete tmp;
+	_size--;
 }
 
-
-int User::GetGrad()
+size_t	User::getSize() const
 {
-    return this->_Grade;
+	return (_size);
 }
 
-void User::SetGrade(int grade)
+t_user	*User::getTop() const
 {
-    this->_Grade = grade;
+	return (_top);
+}
+
+t_user	*User::getBot() const
+{
+	return (_bot);
+}
+
+t_user	*User::getUserByIndex(int index) const
+{
+	t_user	*tmp;
+	int		i;
+
+	if (index < 0 || index >= _size)
+		return (NULL);
+	tmp = getTop();
+	else if (index == 0)
+		return (tmp);
+	i = 1;
+	while (i < _size)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (tmp);
+}
+
+t_user	*User::getUserByUUID(const std::string& uuid) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (uuid == tmp.uuid)
+			return (tmp);
+		tmp = tmp->next;
+		i++;
+	}
+	return (NULL);
+}
+
+t_user	*User::getUserByUUID(const std::string& uuid, int *i) const
+{
+	t_user	*tmp;
+
+	tmp = getTop();
+	while ((*i) < _size)
+	{
+		if (uuid == tmp.uuid)
+			return (tmp);
+		tmp = tmp->next;
+		(*i)++;
+	}
+	return (NULL);
+}
+
+t_user	*User::getUserByName(const std::string& name) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (name == tmp.name)
+			return (tmp);
+		tmp = tmp->next;
+		i++;
+	}
+	return (NULL);
+}
+
+t_user	*User::getUserByNick(const std::string& nick) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (nick == tmp.nick)
+			return (tmp);
+		tmp = tmp->next;
+		i++;
+	}
+	return (NULL);
+}
+
+t_user	*User::getUserByPass(const std::string& pass) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (pass == tmp.pass)
+			return (tmp);
+		tmp = tmp->next;
+		i++;
+	}
+	return (NULL);
+}
+
+bool	User::empty() const
+{
+	return (_top == NULL);
+}
+
+bool	User::hasUUID(const std::string& uuid) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (uuid == tmp.uuid)
+			return (true);
+		tmp = tmp->next;
+		i++;
+	}
+	return (false);
+}
+
+bool	User::hasName(const std::string& name) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (name == tmp.name)
+			return (true);
+		tmp = tmp->next;
+		i++;
+	}
+	return (false);
+}
+
+bool	User::hasNick(const std::string& nick) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (nick == tmp.nick)
+			return (true);
+		tmp = tmp->next;
+		i++;
+	}
+	return (false);
+}
+
+bool	User::hasPass(const std::string& pass) const
+{
+	t_user	*tmp;
+	int		i;
+
+	tmp = getTop();
+	i = 0;
+	while (i < _size)
+	{
+		if (pass == tmp.pass)
+			return (true);
+		tmp = tmp->next;
+		i++;
+	}
+	return (false);
 }
 
 //===============
