5ad65e8bbd3903bdc87ac83b8ed0e79a666074d0
PhoenixNoir <kevin.philotas.developpement@gmail.com>
2025-09-11 11:45:48 +0200

    - Debut d'ecriture du bot en externe

---
+++ irc (OBJ)/bot/inc/botinc.hpp
@@ -0,0 +1,24 @@
+#ifndef BOTINC_HPP
+# define BOTINC_HPP
+
+//HPP
+# include "botparam.hpp"
+# include "botcolor.hpp"
+
+# include <iostream>
+//#include <iomanip>
+//#include <string>
+//#include <limits>
+//#include <cstring>
+//#include <arpa/inet.h>
+//#include <sstream>
+//#include <unistd.h>
+//#include <cstdlib>
+# include <signal.h>
+
+//Lib custom
+# include "bot.hpp"
+# include "botutils.hpp"
+
+
+#endif //BOTINC_HPP
